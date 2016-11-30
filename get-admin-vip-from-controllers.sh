2f11df39aef3ad5cd976f6cc016f587fed529d82
LarsErikP <pedersen.larserik@gmail.com>
2016-11-30 10:52:48 +0100

    Print error message if a host is unreachable

--- get-admin-vip-from-controllers.sh
+++ get-admin-vip-from-controllers.sh
@@ -28,28 +28,31 @@ echo
 for HOST in $CONTROLLERS; do
   echo $HOST
   echo '==============='
-  SERVICES=$(ssh $HOST "ip a | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[1]\.[1][0-9]{2}'")
-  for SERVICE in $SERVICES; do
-    case $SERVICE in
-      $KEYSTONE_IP) echo "KEYSTONE ($SERVICE)"
-      ;;
-      $GLANCE_IP)   echo "GLANCE   ($SERVICE)"
-      ;;
-      $NEUTRON_IP)  echo "NEUTRON  ($SERVICE)"
-      ;;
-      $NOVA_IP)     echo "NOVA     ($SERVICE)"
-      ;;
-      $CINDER_IP)   echo "CINDER   ($SERVICE)"
-      ;;
-      $HEAT_IP)     echo "HEAT     ($SERVICE)"
-      ;;
-      $RABBITMQ_IP) echo "RABBITMQ ($SERVICE)"
-      ;;
-      $MYSQL_IP)    echo "MYSQL    ($SERVICE)"
-      ;;
-      $MEMCACHE_IP) echo "MEMCACHE ($SERVICE)"
-      ;;
-    esac
-  done
+  SERVICES=$(ssh $HOST "ip a | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[1]\.[1][0-9]{2}'" 2> /dev/null)
+  if [ ! -z "$SERVICES" ]; then
+    for SERVICE in $SERVICES; do
+      case $SERVICE in
+        $KEYSTONE_IP) echo "KEYSTONE ($SERVICE)"
+        ;;
+        $GLANCE_IP)   echo "GLANCE   ($SERVICE)"
+        ;;
+        $NEUTRON_IP)  echo "NEUTRON  ($SERVICE)"
+        ;;
+        $NOVA_IP)     echo "NOVA     ($SERVICE)"
+        ;;
+        $CINDER_IP)   echo "CINDER   ($SERVICE)"
+        ;;
+        $HEAT_IP)     echo "HEAT     ($SERVICE)"
+        ;;
+        $RABBITMQ_IP) echo "RABBITMQ ($SERVICE)"
+        ;;
+        $MYSQL_IP)    echo "MYSQL    ($SERVICE)"
+        ;;
+        $MEMCACHE_IP) echo "MEMCACHE ($SERVICE)"
+        ;;
+      esac
+    done
+  else echo "Host is unreachable"
+  fi
   echo
 done
